c5658c8b24ae54d19082c2a35ad9ec76726992a9
Mark Woo <mark.woo@getcruise.com>
2020-08-14 11:54:12 -0700

    add cat file

---
+++ wordlists/atmosphere.js
@@ -0,0 +1,17 @@
+const ATMOSPHERE_LIST = [
+"declivity",
+"fallow",
+"watershed",
+"peninsula",
+"equinox",
+"tributary",
+"doldrums",
+"atmosphere",
+"acclivity",
+"littoral",
+"estuary",
+"ridge",
+"solstice",
+"maritime",
+]
+if (exports) { exports.ATMOSPHERE_LIST = ATMOSPHERE_LIST;}
